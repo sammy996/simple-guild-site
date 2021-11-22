96c8eebf14ab6eb3f377ba2b7dd4f5ff2951715f
Samuel Moraes <sammy@DESKTOP-9ODTR7K.localdomain>
2021-11-22 19:04:57 -0300

    home page

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,27 @@
+import { useState } from 'react';
+
 function Home() {
-  return <div>Home</div>
+  return (
+    <div>
+      <h2>Home</h2>
+      <Counter/>
+    </div>
+  )
 }
+
+function Counter() {
+  const [contador, setCounter] = useState(1);
+
+  function addCounter() {
+    setCounter(contador + 1);
+  }
+
+  return (
+    <div>
+      <div>{contador}</div>
+      <button onClick={addCounter}>Adiconar</button>
+    </div>
+  )
+}
+
+export default Home()
